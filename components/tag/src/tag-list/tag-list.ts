c7f72ddcf63fc0b70d2bda608f970773d5cf0784
rowa-audil <rowa.audil@dynatrace.com>
2020-01-14 09:31:05 +0100

    fix(tag-list): Fixes bug which displayed '0 More...' when no tags are hidden.

--- components/tag/src/tag-list/tag-list.ts
+++ components/tag/src/tag-list/tag-list.ts
@@ -96,6 +96,9 @@ export class DtTagList implements AfterContentInit, OnDestroy {
   /** @internal Whether user wants all tags to be shown */
   _showAllTags = false;
 
+  /** @internal Whether the x more button should be displayed */
+  _displayMoreBtn = false;
+
   constructor(
     private _viewportResizer: DtViewportResizer,
     private _changeDetectorRef: ChangeDetectorRef,
@@ -125,6 +128,7 @@ export class DtTagList implements AfterContentInit, OnDestroy {
               this._isOneLine = true;
             }
           }
+          this._toDisplayMoreButton();
           this._changeDetectorRef.markForCheck();
         });
       this._tagAddElements.changes
@@ -195,6 +199,17 @@ export class DtTagList implements AfterContentInit, OnDestroy {
       subscribtion.unsubscribe();
     });
     this._tagAddSubscriptions = [];
+  }
+
+  /**
+   * @internal evaluates whether to display the x more button
+   */
+  _toDisplayMoreButton(): void {
+    if (!this._isOneLine && !this._showAllTags && this._hiddenTagCount > 0) {
+      this._displayMoreBtn = true;
+    } else {
+      this._displayMoreBtn = false;
+    }
   }
 }
 
